307e152b41860b74a4ec3718adb690a70c3630d5
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-07-05 11:07:17 +0700

    feat: Make responsive for navbar and search bar on any devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                 height={1080}
                 className="h-[40px] lg:h-[69px] w-[40px] lg:w-[61px] "
               />
-              <p className="text-white font-montserrat-b hidden lg:block text-2xl">
+              <p className="text-white font-bold hidden lg:block text-xl 2xl:text-2xl">
                 SEA Cinema
               </p>
             </Link>
@@ -34,42 +34,41 @@ const Navbar = () => {
             <SearchBar />
           </li>
           <button
-            className="flex relative lg:hidden"
+            className="flex relative 2xl:hidden"
             onClick={() => setPopUp((prev) => !prev)}
           >
-            <HamburgerIcon style="w-7 h-7 fill-white" />
+            <HamburgerIcon style="w-7 h-7 md:w-10 fill-white" />
           </button>
           <div
             className={`flex-col ${
               popUp
                 ? "scale-100 opacity-100"
                 : "scale-0 opacity-0 pointer-events-none"
-            } transition duration-300 lg:flex-row bg-black lg:pointer-events-auto lg:bg-transparent absolute top-16 pt-2.5 pb-5 px-5 lg:p-0 lg:scale-100  lg:opacity-100 rounded-xl text-center right-6 lg:static lg:flex items-center justify-center gap-7 text-sm lg:text-base`}
+            } transition duration-300 2xl:flex-row bg-black 2xl:pointer-events-auto 2xl:bg-transparent absolute top-16 pt-2.5 pb-5 px-5  md:px-10 2xl:p-0 2xl:scale-100 lg:px-20 2xl:opacity-100 rounded-xl text-center right-6 md:right-14 lg:top-24 2xl:static 2xl:flex items-center justify-center gap-7 text-sm xl:text-lg`}
           >
-            <li className="py-2.5">
-              <Link href="/movies" className="text-white font-montserrat-b">
+            <li className="py-2.5 md:py-3 lg:py-4">
+              <Link href="/movies" className="text-white font-bold">
                 Movies
               </Link>
             </li>
-            <li className="py-2.5">
-              <Link
-                href="/transactions"
-                className="text-white font-montserrat-b"
-              >
+            <li className="py-2.5 md:py-3 lg:py-4">
+              <Link href="/transactions" className="text-white font-bold">
                 Transaction
               </Link>
             </li>
-            <li className="py-2.5">
-              <Link href="/watchlist" className="text-white font-montserrat-b">
+            <li className="py-2.5 md:py-3 lg:py-4">
+              <Link href="/watchlist" className="text-white font-bold">
                 Watchlist
               </Link>
             </li>
-            <Button color="trans-red">
-              <div className="flex items-center gap-2">
-                <p>Sign Out</p>
-                <Avatar />
-              </div>
-            </Button>
+            <li className="py-1">
+              <Button color="trans-red">
+                <div className="flex items-center gap-2">
+                  <p>Sign Out</p>
+                  <Avatar />
+                </div>
+              </Button>
+            </li>
           </div>
         </ul>
       </div>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,16 +4,16 @@ import SearchIcon from "./icons/SearchIcon";
 const SearchBar = () => {
   return (
     <div className="flex  items-center justify-center border border-gray rounded-xl py-1 lg:py-1.5">
-      <p className="hidden px-16 text-white font-montserrat-sb items-center justify-center lg:flex border-r border-gray">
+      <p className="hidden px-16 text-white text-sm lg:text-base font-semibold items-center justify-center md:flex border-r border-gray">
         Age
       </p>
-      <p className="px-16 hidden text-white font-montserrat-sb items-center justify-center lg:flex border-r border-gray">
+      <p className="px-16 hidden text-white text-sm lg:text-base font-semibold items-center justify-center md:flex border-r border-gray">
         Title
       </p>
-      <div className="px-10 lg:px-12 text-white text-sm lg:text-base font-semibold items-center justify-center flex lg:gap-6 gap-4">
-        <p className="hidden lg:flex">Price</p>
-        <p className="lg:hidden flex">Search</p>
-        <SearchIcon style="fill-white w-4 h-4 lg:w-5 lg:h-5" />
+      <div className="px-10 md:px-12 text-white text-sm lg:text-base font-semibold items-center justify-center flex md:gap-6 gap-4">
+        <p className="hidden md:flex">Price</p>
+        <p className="md:hidden flex">Search</p>
+        <SearchIcon style="fill-white w-4 h-4 md:w-5 md:h-5" />
       </div>
     </div>
   );
