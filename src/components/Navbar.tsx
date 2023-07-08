aa836dea22abcc49f9ca5317c9356187cb821360
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-07-09 01:28:18 +0700

    feat: Remake responsive on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     <nav className="fixed z-20 inset-0 flex w-full h-[60px] lg:h-[100px] bg-soft-black">
       {/* container */}
       <div className="flex items-center justify-around px-2 lg:px-[20px] py-3 lg:py-8 w-full">
-        <ul className="flex items-center justify-around gap-1 lg:gap-8 w-full">
+        <ul className="flex items-center justify-between px-8 md:px-14 lg:px-16 xl:px-20 2xl:px-20 gap-1 lg:gap-8 w-full">
           {/* Logo */}
           <li>
             <Link href="/" className="flex  items-center justify-between gap-4">
@@ -37,9 +37,9 @@ const Navbar = () => {
               </p>
             </Link>
           </li>
-        
+
           <button
-            className="flex relative 2xl:hidden"
+            className="flex relative xl:hidden"
             onClick={() => setPopUp((prev) => !prev)}
           >
             <HamburgerIcon style="w-7 h-7 md:w-10 fill-white" />
@@ -49,13 +49,13 @@ const Navbar = () => {
               popUp
                 ? "scale-100 opacity-100"
                 : "scale-0 opacity-0 pointer-events-none"
-            } transition duration-300 2xl:flex-row bg-black 2xl:pointer-events-auto 2xl:bg-transparent absolute top-16 pt-2.5 pb-5 px-5  md:px-10 2xl:p-0 2xl:scale-100 lg:px-16 2xl:opacity-100 rounded-xl text-center right-6 md:right-14 lg:top-24 2xl:static 2xl:flex items-center justify-center gap-7 text-sm xl:text-lg`}
+            } transition duration-300 xl:flex-row bg-black xl:pointer-events-auto xl:bg-transparent absolute top-16 pt-2.5 pb-5 px-10 md:px-14 xl:p-0 xl:scale-100 lg:px-16 xl:opacity-100 rounded-xl text-center right-6 md:right-14 lg:top-24 xl:static xl:flex items-center justify-center gap-7 text-sm xl:text-lg`}
           >
             <li className="py-2.5 md:py-3 lg:py-4">
               <Link href="/movies" className="text-white font-bold">
                 Movies
               </Link>
-           </li>
+            </li>
             <li className="py-2.5 md:py-3 lg:py-4">
               <Link href="/booking" className="text-white font-bold">
                 Booking
