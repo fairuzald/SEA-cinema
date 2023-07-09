897d6c1086a6529aaf6008d7e8288356e3222521
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-07-09 19:01:40 +0700

    feat: Make homepage become dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Carousel from "@/components/Carousel";
 import Image from "next/image";
 import Link from "next/link";
 import getCurrentUser from "./actions/getCurrentuser";
+export const dynamic = "force-dynamic"
 
 // Function to fetch movies data from the API
 async function getMovies() {
@@ -36,7 +37,6 @@ export default async function Home() {
     releaseDate.setHours(0, 0, 0, 0);
     return releaseDate >= fourWeeksAgo && releaseDate <= currentDate;
   });
-  const currrentUser = await getCurrentUser();
   return (
     <main className="text-red-500 bg-background min-h-screen">
       {/* Background section */}
