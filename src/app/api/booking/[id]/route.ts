12e220a7e15e87da176b88d229a0acde82a46489
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-07-14 08:27:39 +0700

    feat: Create add logic to delete history without doing decrement balance for booking api

--- src/app/api/booking/[id]/route.ts
+++ src/app/api/booking/[id]/route.ts
@@ -31,6 +31,27 @@ export async function DELETE(
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 
+  // Check if the booking has expired
+  const currentDate = new Date(); 
+  const targetDate = new Date(booking.watchDate); 
+  const [hours, minutes] = booking.watchTime.split(':').map(Number);
+  // Sets the target time to the given time, but with the same date as the current date
+  targetDate.setHours(hours);
+  targetDate.setMinutes(minutes);
+  targetDate.setSeconds(0);
+  targetDate.setMilliseconds(0);
+  const isExpired = targetDate.getTime() < currentDate.getTime()
+
+  if (isExpired) {
+    // If booking has expired, return a response indicating it's expired
+    await prisma.transaction.delete({ where: { id: id } });
+
+    return NextResponse.json(
+      { message: "Booking has expired" },
+      { status: 200 }
+    );
+  }
+
   // Return the user's balance
   const updatedUser = await prisma.user.update({
     where: { id: currentUser.id },
